e467baf38e331fd00430746940b7e4cbce4e53cd
beNiceFam <133543297+beNiceFam@users.noreply.github.com>
2023-09-08 18:02:15 -0300

    Add files via upload

---
+++ cleanish.sh
@@ -0,0 +1,29 @@
+. vars.sh
+
+echo "Bulk cache in the directory? y/n/e"
+
+shopt -s nocasematch; read -r aim;
+
+if [ "$aim" = "y" ]; then
+    $list
+    $bulk
+fi
+
+if [ "$aim" = "e" ]; then
+    exit 0
+fi
+
+shopt -u nocasematch;
+
+echo "Clean all Xorg's logs? y/n/e"
+
+shopt -s nocasematch; read -r aim;
+
+if [ "$aim" = "y" ]; then
+    > ~/.xsession-errors
+    > ~/.xsession-errors.old
+fi
+
+shopt -u nocasematch;
+
+echo "Thanks for using cleanish!"; exit 0
