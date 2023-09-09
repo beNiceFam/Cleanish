a2cc6ff4bf34be7b7bc4a84b60626c6992bff4d5
beNiceFam <133543297+beNiceFam@users.noreply.github.com>
2023-09-08 21:00:14 -0300

    Update vars.sh
    The list variable is pointless upon executing. In the prompt it won't print anything at all. So I removed it

--- vars.sh
+++ vars.sh
@@ -1,4 +1,3 @@
 # variables regarding file manipulation
 
 bulk=$(find . -type f -iname "*cache*" -delete && find . -type d -iname "*cache*" -exec rm -rf {} +)
-list=$(find . -type f -iname "*cache*" | grep -i cache && find . -type d -iname "*cache*" | grep -i cache)
